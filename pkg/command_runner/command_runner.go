02dcdc8be10578379353455a4135bb2325fe8bc6
gingersamurai <nazimmalyshev47@gmail.com>
2023-05-17 22:48:25 +0300

    created command runner

---
+++ pkg/command_runner/command_runner.go
@@ -0,0 +1,31 @@
+package command_runner
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+)
+
+type CommandRunner struct{}
+
+func NewCommandRunner() CommandRunner {
+	return CommandRunner{}
+}
+
+func (cr CommandRunner) RunCommand(command, dir string) (string, string, int, error) {
+	cmd := exec.Command("bash", "-c", command)
+	cmd.Dir = dir
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Start()
+	if err != nil {
+		return "", "", 0, err
+	}
+	err = cmd.Wait()
+	if err != nil {
+		return "", "", 0, err
+	}
+	fmt.Println(cmd.Process.Pid)
+	return stdout.String(), stderr.String(), cmd.ProcessState.ExitCode(), nil // вернуть код выхода
+}
